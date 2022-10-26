e33f43ea48630fdd4ea0a471ac2ba7a4f28ff817
Ef-Lina <113369142+Ef-Lina@users.noreply.github.com>
2022-10-26 15:12:01 +0300

    кк

--- Информатика/3/123.py
+++ Информатика/3/123.py
@@ -1,6 +1,5 @@
 class Node:
-    def __init__(self, value,
-                 prev_pointer=None, next_pointer=None):
+    def __init__(self, value, prev_pointer=None, next_pointer=None):
         self.set_value(value)
         self.set_prev(prev_pointer)
         self.set_next(next_pointer)
@@ -41,18 +40,28 @@ class List:
             self._finish_pointer = self._start_pointer
             self._length = 1
         else:
-            self._finish_pointer.set_next(Node(value,
-                                               self._finish_pointer))
+            self._finish_pointer.set_next(Node(value,self._finish_pointer))
             self._finish_pointer = self._finish_pointer.get_next()
             self._length += 1
 
-    def __getitem__(self, i): 
+    def multiappend(self,*args): # множественное дополнение работает 
+        for value in args:
+            if self._length == 0:
+                self._start_pointer = Node(value) 
+                self._finish_pointer = self._start_pointer  
+                self._length = 1                 
+            else:
+                self._finish_pointer.set_next(Node(value, self._finish_pointer))
+                self._finish_pointer = self._finish_pointer.get_next()
+                self._length += 1
+
+    def __getitem__(self, i): # работает /была ошибка из-за недоопределенности середины
         if i < 0 or i > self._length:
             return False
 
-        elif i >= self._length/2:
+        if i >= self._length/2:
             curr_pointer = self._finish_pointer
-            for j in range(self._length, i, -1):
+            for j in range(self._length, i+1, -1):
                 curr_pointer = curr_pointer.get_prev()
             return curr_pointer.get_value()
         else:
@@ -61,41 +70,51 @@ class List:
                 curr_pointer = curr_pointer.get_next()
             return curr_pointer.get_value()
 
-    def __pop__(self, i): 
-        if i < 0 or i >= self._length:
-            return False
-        
-        elif i >= self._length/2:
-            curr_pointer = self._finish_pointer
-            for j in range(self._length, i, -1):
-                curr_pointer = curr_pointer.get_prev()
-            return curr_pointer.get_value()
-            self[i-1].set_next(self[i+1].get_value())
-            self[i+1].set_prev(self[i-1].get_value())
-            for j in range (i+1, self._length):
-                self[j] = self[j+1]
-            self._length -= 1
-    def sum(self, other):
-        if other._length == 0:
-            return self
-        else:
-            self._finish_pointer.set_next(other._start_pointer())
-            for i in range(other._length):
-                self._finish_pointer = other[i]
-                self._length += 1
-            return self
-
-
     def __str__(self):
         arr = []
         for i in range(self._length):
             arr.append(str(self[i]))
         return "[" + ", ".join(arr) + "]"
 
+    def pop(self, i):
+
+        if i < 0 or i >= self._length:
+            return False
+        elif i >= self._length/2:
+            curr_pointer = self._finish_pointer
+            for j in range(self._length, i+1, -1):
+                curr_pointer = curr_pointer.get_prev()
+            return curr_pointer.get_value()    
+        else:
+            curr_pointer = self._start_pointer
+            for j in range(i):
+                curr_pointer = curr_pointer.get_next()
+            return curr_pointer.get_value()
+
+        point = self._start_pointer
+        for j in range(self._length):
+            if i!=j:
+                self._start_pointer.set_value(point.get_value)
+            elif j == self._length:
+                self._start_pointer.set_next(None)
+                self._length -=1
+            point = point.set_next()
+
 
 A = List()
 for i in range(5):
     A.append(i)
-for element in A:
-    print(element)
+
+B = List()
+B.multiappend(1,2,3,4,5,6,7,8,9,10) # проверка multiappend
+
+print(A[3]) # проверка __getitem__ 
+
+print(B.pop(3))
+print(B)
+
+for i in range(A._length):
+    print(A[i])
 print(A)
+
+input()
